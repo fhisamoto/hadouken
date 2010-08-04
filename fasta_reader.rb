5db878abcc8722f854295747f0a41ed1ec23f606
Fabio Hisamoto <fhisamoto@gmail.com>
2010-08-04 00:55:42 -0300

    reducer

--- fasta_reader.rb
+++ fasta_reader.rb
@@ -7,25 +7,34 @@
 require 'rubygems'
 require 'wukong'
 
-class FastaReaderMapper < Wukong::Streamer::Base
-  
-  def stream
-    before_stream
-    while not $stdin.eof?
-      $stdin.each { |l| break if l =~ /^\s*>(.*)/ }
-      seq = {
-        :id => $1, 
-        :data => lambda { 
-          r = $stdin.gets('>').chomp('>')
-          $stdin.ungetc('>'[0]) unless $stdin.eof?
-          r.gsub("\n", "").strip }.call 
-        } unless $stdin.eof?
-      emit seq
-    end
-    after_stream
-  end
+module FastaReader
 
+  class Mapper < Wukong::Streamer::Base
+    def stream
+      before_stream
+      while not $stdin.eof?
+        $stdin.each { |l| break if l =~ /^\s*>(.*)/ }
+          
+        seq = [ $1, 
+           lambda { 
+            r = $stdin.gets('>').chomp('>')
+            $stdin.ungetc('>'[0]) unless $stdin.eof?
+            r.gsub("\n", "").strip }.call 
+          ] unless $stdin.eof?
+        emit seq
+      end
+      after_stream
+    end
+    
+  end
+  
+  class Reducer < Wukong::Streamer::ListReducer
+    def finalize
+      yield [ key, ">> #{values.map(&:last).join(":")}" ]
+    end
+  end
+      
 end
 
-Wukong::Script.new(FastaReaderMapper, nil).run
+Wukong::Script.new(FastaReader::Mapper, FastaReader::Reducer).run
 
